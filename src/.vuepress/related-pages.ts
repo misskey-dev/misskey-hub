bd0b063014af1e09d1d1ad8e0b5091c5b6943c66
syuilo <Syuilotan@yahoo.co.jp>
2021-11-02 19:00:51 +0900

    Resolve #17

--- src/.vuepress/related-pages.ts
+++ src/.vuepress/related-pages.ts
@@ -1,14 +1,35 @@
-import { App } from 'vuepress';
+import { App, Page } from 'vuepress';
 
 export async function getRelatedPages(app: App) {
+	const getFrontmatterRelatedPages = (page: Page): string[] => {
+		return (page.frontmatter.relatedPages || []) as string[];
+	};
+
+	const normalizeLink = (link: string): string => {
+		return link.replace('.md', '.html');
+	};
+
 	for (let i = 0; i < app.pages.length; i++) {
 		const page = app.pages[i];
-		const frontmatterRelatedPages = (page.frontmatter.relatedPages || []) as string[];
 
-		let relatedPages = frontmatterRelatedPages.map(link => app.pages.find(p => p.path === link.replace('.md', '.html'))).filter(x => x != null);
+		let relatedPages = getFrontmatterRelatedPages(page)
+			.map(link => app.pages.find(p => p.path === normalizeLink(link)))
+			.filter(x => x != null);
+
 		relatedPages = [...relatedPages, ...page.links
-				.map(link => app.pages.find(p => p.path === link.absolute.replace('.md', '.html')))
-				.filter(x => x != null && !relatedPages.some(y => y.path === x.path))];
+			.map(link => app.pages.find(p => p.path === normalizeLink(link.absolute)))
+			.filter(x => x != null)
+		];
+
+		relatedPages = [...relatedPages, ...app.pages
+			.filter(p =>
+				getFrontmatterRelatedPages(p).some(link => normalizeLink(link) === page.path) ||
+				p.links.some(link => normalizeLink(link.absolute) === page.path))
+		];
+
+		relatedPages = relatedPages.filter((page, i, self) => i === self.findIndex((p) => (
+			p.path === page.path
+		))); // 重複削除
 
 		if (relatedPages.length === 0) continue;
 
