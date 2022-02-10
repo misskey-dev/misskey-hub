4c5de976bd689b796802d4c3d6d3de356c93b5d6
syuilo <Syuilotan@yahoo.co.jp>
2022-02-10 20:48:12 +0900

    Update config.ts

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -78,6 +78,7 @@ export default defineUserConfig<DefaultThemeOptions>({
 								'/docs/features/mfm',
 								'/docs/features/custom-emoji',
 								'/docs/features/drive',
+								'/docs/features/charts',
 								'/docs/features/online-status',
 								'/docs/features/share-form',
 								'/docs/features/thread-mute',
