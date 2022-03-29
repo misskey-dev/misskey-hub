9afffc5eda42e935894d8daeac528ffe736478cd
Ryu jongheon <lptprjh@gmail.com>
2022-03-29 22:28:25 +0900

    Update year 2021 to 2022 (#57)

--- src/.vuepress/theme/layouts/Layout.vue
+++ src/.vuepress/theme/layouts/Layout.vue
@@ -13,7 +13,7 @@
 	<template #page-bottom>
 		<MkRelatedPages/>
 		<MkDots :class="$style.footerDots"/>
-		<div :class="$style.footer">Copyright (c) 2021 syuilo and other contributors</div>
+		<div :class="$style.footer">Copyright (c) 2022 syuilo and other contributors</div>
 	</template>
 </Layout>
 
