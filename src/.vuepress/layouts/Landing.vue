6636d35a642b82c60a424899585f9e76828c35c2
Kainoa Kanter <44733677+ThatOneCalculator@users.noreply.github.com>
2022-05-05 22:42:57 -0700

    Improved English (#81)
    * Merge conflict
    
    * Fix

--- src/.vuepress/layouts/Landing.vue
+++ src/.vuepress/layouts/Landing.vue
@@ -123,7 +123,7 @@
 							<div class="description">{{ frontmatter.sections.features.list.thread.description }}</div>
 						</div></div>
 						<div v-fade-in class="item"><div class="content">
-							<img src="/top-features-widgets.png" class="img">
+							<img src="/top-features-widgets.png" class="img widgets" alt="Widgets">
 							<div class="title">{{ frontmatter.sections.features.list.widgets.title }}</div>
 							<div class="description">{{ frontmatter.sections.features.list.widgets.description }}</div>
 						</div></div>
