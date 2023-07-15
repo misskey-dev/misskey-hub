67c42dc9cc48b902c22ed7949e4f465f38951f0a
syuilo <syuilo@users.noreply.github.com>
2023-07-15 11:07:57 +0000

    deploy: 8cb341fb18ee7f40662586e50eea490560d8b71f

---
+++ assets/mention.html.035e91c8.js
@@ -0,0 +1,1 @@
+const e=JSON.parse('{"key":"v-e6674fc2","path":"/ko/docs/features/mention.html","title":"\uBA58\uC158","lang":"ko-KR","frontmatter":{},"excerpt":"","headers":[],"git":{"updatedTime":1689418981000,"contributors":[{"name":"Hoto Ras","email":"jdh59681@gmail.com","commits":1}]},"filePathRelative":"ko/docs/features/mention.md"}');export{e as data};
